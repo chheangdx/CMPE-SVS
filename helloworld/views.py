b1564395fe3fd42ef420ac3e6a27e2d885335874
Chheang Duong <chheangduong@yahoo.com>
2016-09-19 23:40:33 -0700

    ::1:: templating hello world

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse("<h1>Hello World!</h1>")
+	template = loader.get_template('helloworld/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))

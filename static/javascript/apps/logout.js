d35c602cb892f95ddb9d107ec72d03e385b0ef41
NCarteazy <carter.nicholas.R@gmail.com>
2017-05-15 18:24:33 -0700

    added register and login logic and cleaned a lot

--- static/javascript/apps/logout.js
+++ static/javascript/apps/logout.js
@@ -6,73 +6,6 @@ app.config(function($httpProvider){
 
 app.controller('settingsCtrl',  ['$scope','$http', function($scope,$http) {
 
-		
-
-	// {
-	// 			bText : "Register",
-	// 			targetForm : {
-	// 			username:{field:"Username", value:"", type:"text", key: 0},
-	// 			password:{field:"Password", value:"", type:"password", key: 1, pwstr : "", pwstrf: function(selfo) {
-	// 					var score = 0;
-	// 				   if (!selfo.value)
-	// 				       return score;
-
-	// 				   // award every unique letter until 5 repetitions
-	// 				   var letters = new Object();
-	// 				   for (var i=0; i<selfo.value.length; i++) {
-	// 				       letters[selfo.value[i]] = (letters[selfo.value[i]] || 0) + 1;
-	// 				       score += 5.0 / letters[selfo.value[i]];
-	// 				   }
-
-	// 				   // bonus points for mixing it up
-	// 				   var variations = {
-	// 				       digits: /\d/.test(selfo.value),
-	// 				       lower: /[a-z]/.test(selfo.value),
-	// 				       upper: /[A-Z]/.test(selfo.value),
-	// 				       nonWords: /\W/.test(selfo.value),
-	// 				   }
-
-	// 				   variationCount = 0;
-	// 				   for (var check in variations) {
-	// 				       variationCount += (variations[check] == true) ? 1 : 0;
-	// 				   }
-	// 				   score += (variationCount - 1) * 10;
-
-				
-	// 					if (score > 80)
-	// 				       selfo.pwstr = "Strong";
-	// 				   else if (score > 60)
-	// 				       selfo.pwstr = "Good";
-	// 				   else if (score >= 25)
-	// 				       selfo.pwstr = "Weak";
-	// 			}},
-	// 			notifyEmailToggle:{field:"Receive Email Notifications?", value:"false", type:"checkbox", key: 2},
-	// 			firstName:{field:"First Name", value:"", type:"text", key: 3},
-	// 			lastName:{field:"Last Name", value:"", type:"text", key: 4},
-	// 			email:{field:"Email", value:"", type:"text", key: 5},
-	// 			stayLoggedIn:{field:"Stay Logged In?", value:"false", type:"checkbox", key: 6}
-	// 			},
-	// 			targetFunction : function() {
-	// 					$scope.dataPrep();
-	// 					console.log("User creating account with username: " + $scope.userForm.username.value);
-	// 					var data = {user: $scope.packet};
-	// 					$scope.shower = data;
-	// 					$http({
-	// 				            method: 'post',
-	// 				            url: '/createAccount',
-	// 				            data: data
-	// 				        }).then(function successCallback(response) {
-	// 				            //successfully got a response
-	// 				            console.log(response);
-	// 				            $scope.message = response.data
-	// 				        }, function errorCallback(response) {
-	// 				            //usually happens when an exception is thrown
-	// 				            console.error(response);
-	// 				        });
-	// 			}
-	// 		}
-
-
 		$scope.enterHandler = function($event) {
 			if ($event.keycode === 13) {
 				$scope.editAccountInformation();
@@ -84,13 +17,6 @@ app.controller('settingsCtrl',  ['$scope','$http', function($scope,$http) {
 			$scope.savedEmail = $scope.currentUser.email
 			var data = {
 				user:$scope.currentUser
-			}
-
-			if($scope.currentUser.email.localeCompare($scope.oldEmail) == 0){
-				data.user.email='BLANK'	
-			}
-			if($scope.currentUser.oldPassword == ""){
-	            data.user.oldPassword = 'BLANK'
 			}
 
 
